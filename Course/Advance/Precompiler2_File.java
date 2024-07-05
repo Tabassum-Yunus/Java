811d3fd2de510919712dbb2e05c76b861f185524
tabassumyunus41@gmail.com <tabassumyunus41@gmail.com>
2024-07-05 12:40:47 +0530

    Commit

---
+++ Course/Advance/Precompiler2_File.java
@@ -0,0 +1,24 @@
+package Week12;
+
+public class W12Q2_File {
+    public void myMethod(){}
+    public void myMethod1(){}
+    public static void main(String[] args) {
+        int a;
+        float d;
+        String str;
+        for(int i=0; i<3; i++){  }      
+        int i=2;
+        while(i!=5)
+        {
+            i++;
+        }        
+        int j=1;
+        do{
+            j++;
+        }while(j!=4);
+        boolean v=true;
+        if(v){}
+        else if(v!=true){}   
+    }
+}
