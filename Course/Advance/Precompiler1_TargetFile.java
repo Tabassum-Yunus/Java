7b696e8d0239f872664cd48554b98e1e33834693
tabassumyunus41@gmail.com <tabassumyunus41@gmail.com>
2024-07-05 12:40:21 +0530

    Commit

---
+++ Course/Advance/Precompiler1_TargetFile.java
@@ -0,0 +1,11 @@
+public class W12Q1_TargetFile 
+{
+    public static void main( String[] args) {
+        System.out.println("My statement terminator: ;");    
+        System.out.println("My single line comments: $");       // single line Comment
+        System.out.println("My Multiline comments: /*  */");    /* Multi line Comment */
+        {
+            System.out.println("This is my block: { } ");
+        }      
+    }
+}
