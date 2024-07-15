d62f0143c5690a30dfb35536ddbce5eae7ce45e6
Tabassum Yunus <tabassumyunus09@gmail.com>
2024-07-15 11:25:28 +0530

    Find if strings are Anagram

---
+++ Practice/Code/Assignment/Aq7_Anagram.java
@@ -0,0 +1,22 @@
+package Assignment;
+
+import Practice.String.*;
+import java.util.Arrays;
+
+public class Aq7_Anagram {
+    public static void main(String[] args) {
+        String str1 = "Parliamnt", str2 = "Partial men";
+        str1 = str1.replace(" ", "");
+        str2 = str2.replace(" ", "");
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        str1 = new String(arr1);
+        str2 = new String(arr2);
+        if(str1.equals(str2))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
