3b896cc03682c91c5aad77a285f8bb27e7262d06
Tabassum Yunus <tabassumyunus09@gmail.com>
2024-08-10 18:44:50 +0530

    Implement Stack

---
+++ Practice/Code/Practice/Collections/J_Stack.java
@@ -0,0 +1,18 @@
+package Practice.Collections;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public class J_Stack {
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        List<Integer> l = Arrays.asList(4,7,2,9,4);
+        System.out.println(l);
+        //l.r(2);
+        System.out.println(l);
+        
+        System.out.println(l.indexOf(9));
+    }
+}
